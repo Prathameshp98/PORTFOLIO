689eded3810964759a4e4ca8163c09ebf8a8570c
Prathamesh Patil <prathamesh.patil@ufaber.com>
2022-09-09 16:06:07 +0530

    initial draft

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+
 import './App.css';
+import SidebarMain from './Components/Sidebar/SidebarMain';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <React.Fragment>
+      <SidebarMain />
+    </React.Fragment>
   );
 }
 
---
+++ src/Components/Sidebar/SidebarMain.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SidebarMain = () => {
+    return (
+        <React.Fragment>
+            
+        </React.Fragment>
+    );
+}
+
+export default SidebarMain
