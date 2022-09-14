1eb9267564c12c3584bd35e2fe87cfb0376029af
Prathamesh Patil <prathamesh.patil@ufaber.com>
2022-09-14 19:12:23 +0530

    sidebar inprogress

--- src/Components/Sidebar/SidebarMain.js
+++ src/Components/Sidebar/SidebarMain.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 import styles from "./SidebarMain.module.scss"
+import profile from "../../assests/images/profile.png"
+import email from "../../assests/images/email.png"
+import github from "../../assests/images/github.png"
+import linkedin from "../../assests/images/linkedin.png"
 
 const SidebarMain = () => {
     return (
@@ -10,20 +14,26 @@ const SidebarMain = () => {
                     <div className={`${styles.container} ${styles.nav_container}`}>
                         <input className={styles.checkbox} type="checkbox" name="" id="" />
                         <div className={styles.hamburger_lines}>
-                        <span className={`${styles.line} ${styles.line1}`}></span>
-                        <span className={`${styles.line} ${styles.line2}`}></span>
-                        <span className={`${styles.line} ${styles.line3}`}></span>
+                            <span className={`${styles.line} ${styles.line1}`}></span>
+                            <span className={`${styles.line} ${styles.line2}`}></span>
+                            <span className={`${styles.line} ${styles.line3}`}></span>
                         </div>  
-                    <div className={styles.logo}>
-                        <h1>Navbar</h1>
-                    </div>
-                    <div className={styles.menu_items}>
-                        <li><a href="..">Home</a></li>
-                        <li><a href="..">about</a></li>
-                        <li><a href="..">blogs</a></li>
-                        <li><a href="..">portfolio</a></li>
-                        <li><a href="..">contact</a></li>
-                    </div>
+                        <div className={styles.menu_items}>
+                            <div className={styles.profile}>
+                                <img className={styles.profile_picture} src={profile} alt="profile-pic"/>
+                            </div>
+                            <li><a href="..">Home</a></li>
+                            <li><a href="..">Skills</a></li>
+                            <li><a href="..">Work</a></li>
+                            <li><a href="..">Blog</a></li>
+                            <li><a href="..">Contact</a></li>
+                            <li className={styles.last_item}></li>
+                            <div className={styles.social_links}>
+                                <img src={email} alt="email-logo" />
+                                <img src={github} alt="github-logo" />
+                                <img src={linkedin} alt="linkedin-logo" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </nav>
