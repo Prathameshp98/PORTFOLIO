5061ff1c4dd1893854b731a848a7ff984dbd8be2
Prathamesh Patil <prathamesh.patil@ufaber.com>
2022-09-16 02:19:25 +0530

    Added favicon, title for website and setup for Home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 import styles from './App.module.scss';
-import SidebarMain from './Components/Sidebar/SidebarMain';
 import background from './assests/images/background.svg'
+import SidebarMain from './Components/Sidebar/SidebarMain';
+import BlogMain from './Components/Blog/BlogMain';
 
 function App() {
   return (
     <React.Fragment>
       <img className={styles.common_background} src={background} alt="common-background" />
       <SidebarMain />
+      <BlogMain />
     </React.Fragment>
   );
 }
