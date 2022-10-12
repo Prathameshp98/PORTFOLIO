916b93372d668bcdbe07c66db677c780a14630fc
Prathamesh Patil <prathamesh.patil@ufaber.com>
2022-10-12 23:31:38 +0530

    creating files for style and arranging render order of main components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import styles from './App.module.scss';
 import background from './assests/images/background.svg'
 import SidebarMain from './Components/Sidebar/SidebarMain';
 import HomeMain from './Components/Home/HomeMain';
+import SkillsMain from './Components/Skills/SkillsMain';
+import WorkMain from './Components/Work/WorkMain';
 import BlogMain from './Components/Blog/BlogMain';
+import ContactMain from './Components/Contact/ContactMain';
 import ScrollDown from './Components/Extras/SrollDown';
 
 function App() {
@@ -13,7 +16,10 @@ function App() {
       <img className={styles.common_background} src={background} alt="common-background" />
       <SidebarMain />
       <HomeMain />
+      <SkillsMain />
+      <WorkMain />
       <BlogMain />
+      <ContactMain />
       <ScrollDown />
     </React.Fragment>
   );
