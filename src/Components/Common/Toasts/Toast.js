9abb35df4b22b878f7b085d80f228dce8e26f62a
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-03-21 14:50:24 +0530

    functionality of toast

--- src/Components/Common/Toasts/Toast.js
+++ src/Components/Common/Toasts/Toast.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import styles from './Toast.module.scss'
 import close from '../../../assests/images/cancel.png'
@@ -7,14 +7,29 @@ import warning from '../../../assests/images/shield-exclamation.png'
 import success from '../../../assests/images/badge-check.png'
 
 const Toast = (props) => {
+
+    const[toast, setToast] = useState(true)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setToast(false)
+        }, 3000);
+        return () => clearTimeout(timer)
+        
+    }, [])
+
+    const toastToggleHandler = () => {
+        setToast(false)
+    }
+
     return (
-        <div className={`${styles.toast__container}`} style={{border: "2px solid " + props.color }}>
+        <div className={`${styles.toast__container}`} style={{border: "2px solid " + props.color, display: toast ? "flex" : "none" }}>
             <img className={`${styles.logo}`} src={success} alt="logo" />
             <div className={`${styles.information}`}>
                 <h3 style={{color: props.color}}>Success</h3>
                 <p>Hello, world! This is a toast message.</p>
             </div>
-            <img className={`${styles.close}`} src={close} alt="close"/>
+            <img className={`${styles.close}`} src={close} alt="close" onClick={toastToggleHandler}/>
         </div>
     )
 }
