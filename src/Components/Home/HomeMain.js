13a4c880ee19a6785e5c84db3edecaa6e86104c2
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-03-30 23:13:24 +0530

    Added download func

--- src/Components/Home/HomeMain.js
+++ src/Components/Home/HomeMain.js
@@ -4,6 +4,10 @@ import styles from './HomeMain.module.scss';
 
 const HomeMain = React.forwardRef((props, ref) => {
 
+    const downloadHandler = async() => {
+        window.location.href = 'https://portfolioapp-prathameshpatil2812.b4a.run/download'
+    }
+
     return (
         <React.Fragment>
             <div ref={ref} className={styles.home} >
@@ -14,7 +18,7 @@ const HomeMain = React.forwardRef((props, ref) => {
                     <br />
                     You can view my <span className={styles.highlight}>Work</span>, follow my <span className={styles.highlight}>Blog</span> and <span className={styles.highlight}>get in Touch</span>with me or email me at <span className={styles.highlight}>prathameshpatil2812@gmail.com</span>.
                 </p>
-                <button className={styles.button}>Download Resume</button>
+                <button className={styles.button} onClick={downloadHandler}>Download Resume</button>
             </div>
         </React.Fragment>
     );
